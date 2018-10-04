125d1bcdcaf80603195e7274778c0592e2147a0f
Claire Gammon <claire.gammon@gmail.com>
2018-10-04 09:06:40 -0600

    asdf

--- tsh.c
+++ tsh.c
@@ -289,21 +289,12 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv)
 {
-//    char *quit_cmd = "quit";
-//    char *jobs_cmd = "jobs";
-//    char *bg_cmd = "bg";
-//    char *fg_cmd = "fg";
-printf("here i am in builtin\n");
+// printf("here i am in builtin\n");
 printf("argv: %s\n", argv[0]);
 
     if( strcmp("quit", argv[0]) == 0) {
         exit(0);
     } else if( strcmp("jobs", argv[0]) == 0) {
-        // printf("do jobs\n");
-//        printf("list all jobs\n");
-//        listjobs(jobs);
-        // printf("list bg jobs\n");
-        // listbgjobs(jobs);
         listjobs(jobs);
         return 1;
     } else if( strcmp("bg", argv[0]) == 0 || strcmp("fg", argv[0]) == 0) {
